914e1aec5a1d37f519431d0dbd860712c411e421
jellywiz <muzaffer.dler@gmail.com>
2022-10-11 01:38:45 +0300

    Add linters

--- main.js
+++ main.js
@@ -4,17 +4,14 @@ const navClose = document.getElementById('close-menu');
 const navPortfolio = document.getElementById('mobile__nav_portfolio');
 const navAbout = document.getElementById('mobile__nav_about');
 const navContact = document.getElementById('mobile__nav_contact');
-
 function showNavbar() {
   mobileNav.classList.remove('hidden');
   navOpen.classList.add('hidden');
 }
-
 function hideNavbar() {
   mobileNav.classList.add('hidden');
   navOpen.classList.remove('hidden');
 }
-
 navOpen.addEventListener('click', showNavbar);
 navClose.addEventListener('click', hideNavbar);
 navPortfolio.addEventListener('click', hideNavbar);
