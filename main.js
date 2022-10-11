53ccf379c658abe3d18141fb42dfb8c5bd95b91a
jellywiz <muzaffer.dler@gmail.com>
2022-10-11 18:01:57 +0300

    Add object in javascript

--- main.js
+++ main.js
@@ -8,6 +8,69 @@ const cardContainer = document.getElementById('card-content');
 const portFolio = document.getElementById('projects-container');
 const buttons = portFolio.getElementsByTagName('button');
 
+const projectInformation = [
+  {
+    id: 0,
+    name: '',
+    description: '',
+    feature: '',
+    image: '',
+    technologies: '',
+    liveVersionLink: '',
+    sourceLink: '',
+  },
+  {
+    id: 1,
+    name: 'Data Dashboard',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    feature: 'This is a very basic suuvey that can be modified at your convinience',
+    image: './img/ImgPlaceholder0.png',
+    technologies: ['HTML', 'inline CSS'],
+    liveVersionLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
+    sourceLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
+  },
+  {
+    id: 2,
+    name: 'Technical Documentation',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    feature: 'A fixed left panel that uses the complete screen hieght and the rest of the screen is for the documentation and detail information',
+    image: './img/ImgPlaceholder2.png',
+    technologies: ['HTML', 'inline CSS'],
+    liveVersionLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
+    sourceLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
+  },
+  {
+    id: 3,
+    name: 'Technical Documentation',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    feature: 'A page that uses all the inpt types, and filters for them ina good shape and send results.',
+    image: './img/ImgPlaceholder1.png',
+    technologies: ['HTML', 'inline CSS'],
+    liveVersionLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
+    sourceLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
+  },
+  {
+    id: 4,
+    name: 'Data Dashboard',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    feature: 'This is a very basic suuvey that can be modified at your convinience',
+    image: './img/ImgPlaceholder0.png',
+    technologies: ['HTML', 'inline CSS'],
+    liveVersionLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
+    sourceLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
+  },
+  {
+    id: 5,
+    name: 'Technical Documentation',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    feature: 'A fixed left panel that uses the complete screen hieght and the rest of the screen is for the documentation and detail information',
+    image: './img/ImgPlaceholder2.png',
+    technologies: ['HTML', 'inline CSS'],
+    liveVersionLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
+    sourceLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
+  },
+];
+
 function showNavbar() {
   mobileNav.classList.remove('hidden');
   navOpen.classList.add('hidden');
