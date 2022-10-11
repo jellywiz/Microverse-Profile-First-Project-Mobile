62468072b513c925abdfbb9dfb5189f4dc0b484c
jellywiz <muzaffer.dler@gmail.com>
2022-10-12 00:23:04 +0300

    add popup html

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const projectInformation = [
     description: '',
     feature: '',
     image: './img/ImgPlaceholder1.png',
-    technologies: '',
+    technologies: ['html', 'bootstrap', 'Ruby'],
     liveVersionLink: '',
     sourceLink: '',
   },
@@ -26,7 +26,7 @@ const projectInformation = [
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     feature: 'This is a very basic suuvey that can be modified at your convinience',
     image: './img/ImgPlaceholder0.png',
-    technologies: ['HTML', 'inline CSS'],
+    technologies: ['html', 'bootstrap', 'Ruby'],
     liveVersionLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
     sourceLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
   },
@@ -36,7 +36,7 @@ const projectInformation = [
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     feature: 'A fixed left panel that uses the complete screen hieght and the rest of the screen is for the documentation and detail information',
     image: './img/ImgPlaceholder2.png',
-    technologies: ['HTML', 'inline CSS'],
+    technologies: ['html', 'bootstrap', 'Ruby'],
     liveVersionLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
     sourceLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
   },
@@ -46,7 +46,7 @@ const projectInformation = [
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     feature: 'A page that uses all the inpt types, and filters for them ina good shape and send results.',
     image: './img/ImgPlaceholder1.png',
-    technologies: ['HTML', 'inline CSS'],
+    technologies: ['html', 'bootstrap', 'Ruby'],
     liveVersionLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
     sourceLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
   },
@@ -56,7 +56,7 @@ const projectInformation = [
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     feature: 'This is a very basic suuvey that can be modified at your convinience',
     image: './img/ImgPlaceholder0.png',
-    technologies: ['HTML', 'inline CSS'],
+    technologies: ['html', 'bootstrap', 'Ruby'],
     liveVersionLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
     sourceLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
   },
@@ -66,7 +66,7 @@ const projectInformation = [
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     feature: 'A fixed left panel that uses the complete screen hieght and the rest of the screen is for the documentation and detail information',
     image: './img/ImgPlaceholder2.png',
-    technologies: ['HTML', 'inline CSS'],
+    technologies: ['html', 'bootstrap', 'Ruby'],
     liveVersionLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
     sourceLink: 'https://github.com/jellywiz/Microverse-Profile-First-Project-Mobile',
   },
@@ -74,18 +74,20 @@ const projectInformation = [
 
 function LoadProjects1() {
   let counter;
+ 
   for (counter = 0; counter < 3; counter += 1) {
+    let tech = 0;
     cardContainer.innerHTML += `<div class="work">
                                   <div style="background-image: url(${projectInformation[counter].image});" class="project${[counter + 1]}">
                                     <div class="project1-card">
                                       <h3 class="project1-title">${projectInformation[counter].name}</h3>
                                       <p class="project1-text">${projectInformation[counter].description}</p>
                                       <ul class="project-skills">
-                                        <li><p class="project-card-item">html</p>
+                                        <li><p class="project-card-item">${projectInformation[counter].technologies[tech++]}</p>
                                         </li>
-                                        <li><p class="project-card-item">bootstrap</p>
+                                        <li><p class="project-card-item">${projectInformation[counter].technologies[tech++]}</p>
                                         </li>
-                                        <li><p class="project-card-item">Ruby</p>
+                                        <li><p class="project-card-item">${projectInformation[counter].technologies[tech++]}</p>
                                         </li>
                                       </ul>
                                       <button class="Project-btn">See Project</button>
@@ -97,17 +99,18 @@ function LoadProjects1() {
 function LoadProjects2() {
   let counter;
   for (counter = 3; counter < projectInformation.length; counter += 1) {
+    let tech = 0;
     cardContainer1.innerHTML += `<div class="work">
                                   <div style="background-image: url(${projectInformation[counter].image});" class="project${[counter + 1]}">
                                     <div class="project1-card">
                                       <h3 class="project1-title">${projectInformation[counter].name}</h3>
                                       <p class="project1-text">${projectInformation[counter].description}</p>
                                       <ul class="project-skills">
-                                        <li><p class="project-card-item">html</p>
+                                        <li><p class="project-card-item">${projectInformation[counter].technologies[tech++]}</p>
                                         </li>
-                                        <li><p class="project-card-item">bootstrap</p>
+                                        <li><p class="project-card-item">${projectInformation[counter].technologies[tech++]}</p>
                                         </li>
-                                        <li><p class="project-card-item">Ruby</p>
+                                        <li><p class="project-card-item">${projectInformation[counter].technologies[tech++]}</p>
                                         </li>
                                       </ul>
                                       <button class="Project-btn">See Project</button>
